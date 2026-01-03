5e444ef334904f27b7bf47bfdcb766ebfbc4dc64
Kreatednotiond <kreatednotion.designs@gmail.com>
2026-01-02 22:15:31 -0600

    Update products.js

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,1 +1,222 @@
+// assets/js/products.js
+
+window.WITTY_PRODUCTS = [
+  // -------------------------
+  // TEES (UNISEX) - $25 S-3XL
+  // -------------------------
+  {
+    id: "witty-tee-quarter-chest",
+    name: "Witty T-Shirt (Quarter Chest)",
+    price: 25,
+    type: "tee",
+    gender: "unisex",
+    sizes: ["S","M","L","XL","2XL","3XL"],
+    colors: [
+      { label: "Black (White wording)", value: "Black (White wording)", img: "assets/product/witty-tee-quarter-chest/black.png" },
+      { label: "Red (White wording)", value: "Red (White wording)", img: "assets/product/witty-tee-quarter-chest/red.png" },
+      { label: "White (Black wording)", value: "White (Black wording)", img: "assets/product/witty-tee-quarter-chest/white-black.png" },
+      { label: "White (Red wording)", value: "White (Red wording)", img: "assets/product/witty-tee-quarter-chest/white-red.png" },
+    ],
+    description: "Unisex tee. Quarter chest logo.",
+    badge: "Unisex"
+  },
+  {
+    id: "big-witty-tee-full-chest",
+    name: "Big Witty Moves T-Shirt (Full Chest)",
+    price: 25,
+    type: "tee",
+    gender: "unisex",
+    sizes: ["S","M","L","XL","2XL","3XL"],
+    colors: [
+      { label: "Black (White wording)", value: "Black (White wording)", img: "assets/product/big-witty-tee-full-chest/black.png" },
+      { label: "Red (White wording)", value: "Red (White wording)", img: "assets/product/big-witty-tee-full-chest/red.png" },
+      { label: "White (Black wording)", value: "White (Black wording)", img: "assets/product/big-witty-tee-full-chest/white-black.png" },
+      { label: "White (Red wording)", value: "White (Red wording)", img: "assets/product/big-witty-tee-full-chest/white-red.png" },
+    ],
+    description: "Unisex tee. Full chest print.",
+    badge: "Unisex"
+  },
+
+  // -------------------------
+  // CREWNECKS (MEN SIZING) - $45 S-3XL
+  // -------------------------
+  {
+    id: "big-witty-crewneck-full-chest",
+    name: "Big Witty Moves Crewneck (Full Chest)",
+    price: 45,
+    type: "crewneck",
+    gender: "men",
+    sizes: ["S","M","L","XL","2XL","3XL"],
+    colors: [
+      { label: "Forest", value: "Forest", img: "assets/product/big-witty-crewneck-full-chest/forest.png" },
+      { label: "Grey", value: "Grey", img: "assets/product/big-witty-crewneck-full-chest/grey.png" },
+      { label: "Navy", value: "Navy", img: "assets/product/big-witty-crewneck-full-chest/navy.png" },
+      { label: "Black", value: "Black", img: "assets/product/big-witty-crewneck-full-chest/black.png" },
+      { label: "Slate", value: "Slate", img: "assets/product/big-witty-crewneck-full-chest/slate.png" },
+    ],
+    description: "Men sizing crewneck. Full chest logo.",
+    badge: "Men sizing"
+  },
+  {
+    id: "witty-crewneck-quarter-chest",
+    name: "Witty Crewneck (Quarter Chest)",
+    price: 45,
+    type: "crewneck",
+    gender: "men",
+    sizes: ["S","M","L","XL","2XL","3XL"],
+    colors: [
+      { label: "Forest", value: "Forest", img: "assets/product/witty-crewneck-quarter-chest/forest.png" },
+      { label: "Grey", value: "Grey", img: "assets/product/witty-crewneck-quarter-chest/grey.png" },
+      { label: "Navy", value: "Navy", img: "assets/product/witty-crewneck-quarter-chest/navy.png" },
+      { label: "Black", value: "Black", img: "assets/product/witty-crewneck-quarter-chest/black.png" },
+      { label: "Slate", value: "Slate", img: "assets/product/witty-crewneck-quarter-chest/slate.png" },
+    ],
+    description: "Men sizing crewneck. Quarter chest logo.",
+    badge: "Men sizing"
+  },
+
+  // -------------------------
+  // HOODIES (MEN SIZING) - $50 S-2XL
+  // -------------------------
+  {
+    id: "big-witty-hoodie-full-chest",
+    name: "Big Witty Moves Hoodie (Full Chest)",
+    price: 50,
+    type: "hoodie",
+    gender: "men",
+    sizes: ["S","M","L","XL","2XL"],
+    colors: [
+      { label: "Black", value: "Black", img: "assets/product/big-witty-hoodie-full-chest/black.png" },
+      { label: "Cloudy Blue", value: "Cloudy Blue", img: "assets/product/big-witty-hoodie-full-chest/cloudy-blue.png" },
+      { label: "Military Olive", value: "Military Olive", img: "assets/product/big-witty-hoodie-full-chest/military-olive.png" },
+      { label: "Ocean Navy", value: "Ocean Navy", img: "assets/product/big-witty-hoodie-full-chest/ocean-navy.png" },
+      { label: "Salmon", value: "Salmon", img: "assets/product/big-witty-hoodie-full-chest/salmon.png" },
+      { label: "Seafoam", value: "Seafoam", img: "assets/product/big-witty-hoodie-full-chest/seafoam.png" },
+      { label: "Slate", value: "Slate", img: "assets/product/big-witty-hoodie-full-chest/slate.png" },
+    ],
+    description: "Men sizing hoodie. Full chest logo.",
+    badge: "Men sizing"
+  },
+  {
+    id: "witty-hoodie-quarter-chest",
+    name: "Witty Hoodie (Quarter Chest)",
+    price: 50,
+    type: "hoodie",
+    gender: "men",
+    sizes: ["S","M","L","XL","2XL"],
+    colors: [
+      { label: "Black", value: "Black", img: "assets/product/witty-hoodie-quarter-chest/black.png" },
+      { label: "Cloudy Blue", value: "Cloudy Blue", img: "assets/product/witty-hoodie-quarter-chest/cloudy-blue.png" },
+      { label: "Military Olive", value: "Military Olive", img: "assets/product/witty-hoodie-quarter-chest/military-olive.png" },
+      { label: "Ocean Navy", value: "Ocean Navy", img: "assets/product/witty-hoodie-quarter-chest/ocean-navy.png" },
+      { label: "Salmon", value: "Salmon", img: "assets/product/witty-hoodie-quarter-chest/salmon.png" },
+      { label: "Seafoam", value: "Seafoam", img: "assets/product/witty-hoodie-quarter-chest/seafoam.png" },
+      { label: "Slate", value: "Slate", img: "assets/product/witty-hoodie-quarter-chest/slate.png" },
+    ],
+    description: "Men sizing hoodie. Quarter chest logo.",
+    badge: "Men sizing"
+  },
+
+  // -------------------------
+  // OVERSIZED HOODIE - $55 S-2XL (MEN SIZING)
+  // -------------------------
+  {
+    id: "witty-oversized-hoodie",
+    name: "Witty Oversized Hoodie",
+    price: 55,
+    type: "oversized",
+    gender: "men",
+    sizes: ["S","M","L","XL","2XL"],
+    colors: [
+      { label: "Black", value: "Black", img: "assets/product/witty-oversized-hoodie/black.png" },
+      { label: "Mineral", value: "Mineral", img: "assets/product/witty-oversized-hoodie/mineral.png" },
+      { label: "Parchment", value: "Parchment", img: "assets/product/witty-oversized-hoodie/parchment.png" },
+      { label: "Pine", value: "Pine", img: "assets/product/witty-oversized-hoodie/pine.png" },
+    ],
+    description: "Oversized hoodie. Men sizing.",
+    badge: "Men sizing"
+  },
+
+  // -------------------------
+  // SWEATPANTS - $45 S-2XL (MEN SIZING)
+  // -------------------------
+  {
+    id: "big-witty-sweatpants",
+    name: "Big Witty Moves Sweatpants",
+    price: 45,
+    type: "sweatpants",
+    gender: "men",
+    sizes: ["S","M","L","XL","2XL"],
+    colors: [
+      { label: "Black", value: "Black", img: "assets/product/big-witty-sweatpants/black.png" },
+      { label: "Cloudy Blue", value: "Cloudy Blue", img: "assets/product/big-witty-sweatpants/cloudy-blue.png" },
+      { label: "Military Olive", value: "Military Olive", img: "assets/product/big-witty-sweatpants/military-olive.png" },
+      { label: "Ocean Navy", value: "Ocean Navy", img: "assets/product/big-witty-sweatpants/ocean-navy.png" },
+      { label: "Salmon", value: "Salmon", img: "assets/product/big-witty-sweatpants/salmon.png" },
+      { label: "Seafoam", value: "Seafoam", img: "assets/product/big-witty-sweatpants/seafoam.png" },
+      { label: "Slate", value: "Slate", img: "assets/product/big-witty-sweatpants/slate.png" },
+    ],
+    description: "Sweatpants. Men sizing.",
+    badge: "Men sizing"
+  },
+
+  // -------------------------
+  // HAT - $30 (JPG, 3 photos)
+  // -------------------------
+  {
+    id: "w-for-witty-snapback",
+    name: "W for Witty Snapback",
+    price: 30,
+    type: "hat",
+    gender: "unisex",
+    sizes: ["One Size"],
+    colors: [
+      { label: "Black", value: "Black", img: "assets/product/w-for-witty-snapback/1.jpg" }
+    ],
+    gallery: [
+      "assets/product/w-for-witty-snapback/1.jpg",
+      "assets/product/w-for-witty-snapback/2.jpg",
+      "assets/product/w-for-witty-snapback/3.jpg",
+    ],
+    description: "Snapback hat.",
+    badge: "Unisex"
+  },
+
+  // -------------------------
+  // VARSITY JACKETS (LIMITED) - $250
+  // Women cropped: S-2XL
+  // Unisex: M-2XL
+  // Each has 2 photos (1 and 2)
+  // -------------------------
+  {
+    id: "all-witty-varsity-womens-cropped",
+    name: "All Witty Varsity Jacket (Women Cropped) - Limited Edition",
+    price: 250,
+    type: "varsity",
+    gender: "women",
+    sizes: ["S","M","L","XL","2XL"],
+    colors: [{ label: "Black/White", value: "Black/White", img: "assets/product/all-witty-varsity-womens-cropped/1.png" }],
+    gallery: [
+      "assets/product/all-witty-varsity-womens-cropped/1.png",
+      "assets/product/all-witty-varsity-womens-cropped/2.png",
+    ],
+    description: "Limited edition cropped varsity jacket (women).",
+    badge: "Limited Edition"
+  },
+  {
+    id: "all-witty-varsity-unisex",
+    name: "All Witty Varsity Jacket (Unisex) - Limited Edition",
+    price: 250,
+    type: "varsity",
+    gender: "unisex",
+    sizes: ["M","L","XL","2XL"],
+    colors: [{ label: "Black/White", value: "Black/White", img: "assets/product/all-witty-varsity-unisex/1.png" }],
+    gallery: [
+      "assets/product/all-witty-varsity-unisex/1.png",
+      "assets/product/all-witty-varsity-unisex/2.png",
+    ],
+    description: "Limited edition varsity jacket (unisex).",
+    badge: "Limited Edition"
+  },
+];
 
